61f334399cf70d512ed0ab3f62ce021ad7b6eba1
Dmitry Dzhus <dima@dzhus.org>
2012-11-27 16:12:04 +0400

    +getRegion, export BBox

--- src/Graphics/Canvas.hs
+++ src/Graphics/Canvas.hs
@@ -7,20 +7,32 @@ Canvas types and functions.
 -}
 
 module Graphics.Canvas
-    ( Canvas
+    ( -- * Canvas basics
+      Canvas
     , makeCanvas
 
+    -- ** Slicing canvas
+    , BBox(..)
+    , getRegion
+
+    -- * Canvas operations
     , Tool(..)
     , roundBrush
+
+    , Pixel(..)
     )
 
 where
+
+import Control.Monad
+import Control.Monad.ST
 
 import Data.Array.Repa as R hiding ((++))
 
 import qualified Data.Vector.Unboxed as VU
 
 import Graphics.Canvas.Base
+import Graphics.Canvas.BBox
 import Graphics.Canvas.Tools
 
 
@@ -35,3 +47,18 @@ makeCanvas :: Int
 makeCanvas height width initial =
     Canvas $ R.fromUnboxed (R.ix2 height width) $
     VU.replicate (height * width) initial
+
+
+getRegion :: BBox
+          -> Canvas
+          -- ^ Source canvas.
+          -> Canvas
+getRegion bbox c@(Canvas arr) =
+    let
+        (ClampedBBox (BBox ((Z :. y0 :. x0), (Z :. y1 :. x1)))) =
+            clampBBox bbox c
+        newArr = R.fromFunction 
+                 (R.ix2 (y1 - y0 + 1) (x1 - x0 + 1)) $
+                 \(Z :. y :. x) -> arr ! (R.ix2 (y + y0) (x + x0))
+    in runST $
+      liftM Canvas $ R.computeUnboxedP newArr
