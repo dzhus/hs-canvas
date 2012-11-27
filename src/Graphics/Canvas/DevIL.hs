a95531e9bdabc351670e3e415795b7a78f045636
Dmitry Dzhus <dima@dzhus.org>
2012-11-27 16:11:34 +0400

    DevIL interface

---
+++ src/Graphics/Canvas/DevIL.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Graphics.Canvas.DevIL
+    ( toImage
+    )
+
+where
+
+import Control.Monad
+import Control.Monad.ST
+
+import Data.Array.Repa as R hiding ((++))
+import Data.Array.Repa.IO.DevIL
+
+import Graphics.Canvas.Base
+
+
+-- | Convert canvas to a DevIL RGB image.
+toImage :: Canvas -> Image
+toImage (Canvas arr) =
+    let
+        (Z :. height :. width) = R.extent arr
+        imgArr = R.fromFunction (Z :. height :. width :. 3)
+                 (\(!Z :. y :. x :. c) ->
+                  let
+                      !(RGBPixel (r, g, b)) = arr ! (R.ix2 y x)
+                  in
+                    case c of
+                      0 -> r
+                      1 -> g
+                      2 -> b)
+    in runST $
+      liftM RGB $ R.computeP imgArr
