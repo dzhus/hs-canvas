865f518b99898b7310e5bc5d629dbebeec2890a4
Dmitry Dzhus <dima@dzhus.org>
2012-12-02 03:29:04 +0400

    Remove excessive bangs & parens

--- src/Graphics/Canvas/DevIL.hs
+++ src/Graphics/Canvas/DevIL.hs
@@ -20,14 +20,14 @@ toImage :: Canvas -> Image
 toImage (Canvas arr) =
     let
         (Z :. height :. width) = R.extent arr
-        imgArr = R.fromFunction (Z :. height :. width :. 3)
-                 (\(!Z :. y :. x :. c) ->
-                  let
-                      !(RGBPixel (r, g, b)) = arr ! (R.ix2 y x)
-                  in
-                    case c of
-                      0 -> r
-                      1 -> g
-                      2 -> b)
+        imgArr = R.fromFunction (Z :. height :. width :. 3) $
+                 \(Z :. y :. x :. c) ->
+                 let
+                     RGBPixel (r, g, b) = arr ! (R.ix2 y x)
+                 in
+                   case c of
+                     0 -> r
+                     1 -> g
+                     2 -> b
     in runST $
       liftM RGB $ R.computeP imgArr
