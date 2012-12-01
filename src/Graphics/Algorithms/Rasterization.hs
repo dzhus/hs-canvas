8dcd9bb081b85c5ab50bb3bd552c3952b3aea155
Dmitry Dzhus <dima@dzhus.org>
2012-12-02 03:04:43 +0400

    +Graphics.Algorithms.Rasterization (Bresenham's algorithm)

---
+++ src/Graphics/Algorithms/Rasterization.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE BangPatterns #-}
+
+{-|
+
+Rasterization algorithms.
+
+-}
+
+module Graphics.Algorithms.Rasterization
+    ( bresenham
+    )
+
+where
+
+import qualified Data.Vector.Unboxed as VU
+
+
+-- | Bersenham's line algorithm.
+--
+-- Produce a vector of points on two-dimensional raster which
+-- approximate a segment between two given points. Points in vector
+-- are ordered from start to end.
+--
+-- Simple point type is used to accomodate to non-canvas/non-Repa users.
+bresenham :: (Int, Int)
+          -- ^ Start point.
+          -> (Int, Int)
+          -- ^ End point.
+          -> VU.Vector (Int, Int)
+bresenham p0@(x0, y0) p1@(x1, y1)
+  | slope > 1 = VU.map pointFlip $ bresenham (pointFlip p0) (pointFlip p1)
+  | otherwise =
+      VU.unfoldr
+           (\(x, y, err) ->
+                let
+                    !newErr' = err + slope
+                    !(newY, newErr) =
+                        if newErr' >= 0.5
+                        then (y + yStep, newErr' - 1.0)
+                        else (y, newErr')
+                in
+                  if x == (x1 + xStep)
+                  then Nothing
+                  else Just ((x, y), (x + xStep, newY, newErr))
+           ) (x0, y0, 0)
+    where
+      pointFlip (x, y) = (y, x)
+      slope = abs $ (fromIntegral $ y1 - y0) / (fromIntegral $ x1 - x0)
+      xStep = if x0 < x1 then 1 else -1
+      yStep = if y0 < y1 then 1 else -1
