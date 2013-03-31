7b8dc3e325a7397027e3e99a46130ac9c0050ea7
Dmitry Dzhus <dima@dzhus.org>
2013-03-31 16:18:56 +0400

    Decrease test canvas size

--- src/Main.hs
+++ src/Main.hs
@@ -9,16 +9,18 @@ import Graphics.Canvas.DevIL
 
 main = do
   (fileName:_) <- getArgs
-  let !c = makeCanvas 10000 10000 (RGBPixel (255, 255, 255))
+  let !c = makeCanvas 1000 1000 (RGBPixel (255, 255, 255))
       !b = roundBrush 40 (RGBPixel (255, 0, 255))
-      !b2 = ellipticBrush 40 0.99 (RGBPixel (0, 255, 255))
+      !b2 = ellipticBrush 40 0.75 (RGBPixel (0, 255, 255))
       !p = pixel (RGBPixel (0, 0, 0))
       !l = line b
       !l2 = line b2
       !lp = line p
       x = (BBox ((Z :. 400 :. 400), (Z :. 520 :. 520)))
-  unsafeApply l ((Z :. 0 :. 0), (Z :. 9999 :. 9999)) c
-  unsafeApply l2 ((Z :. 0 :. 9999), (Z :. 9999 :. 0)) c
-  unsafeApply lp ((Z :. 5000 :. 0), (Z :. 5000 :. 9999)) c
-  unsafeApply lp ((Z :. 0 :. 5000), (Z :. 9999 :. 5000)) c
+  unsafeApply l ((Z :. 0 :. 0), (Z :. 999 :. 999)) c
+  unsafeApply l2 ((Z :. 0 :. 999), (Z :. 999 :. 0)) c
+  unsafeApply b (Z :. 500 :. 500) c
+  unsafeApply b2 (Z :. 500 :. 500) c
+  unsafeApply lp ((Z :. 0 :. 500), (Z :. 999 :. 500)) c
+  unsafeApply lp ((Z :. 500 :. 0), (Z :. 500 :. 999)) c
   runIL $ writeImage fileName (toImage c)
